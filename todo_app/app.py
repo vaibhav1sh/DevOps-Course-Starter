304d5e23425a14c4122d5672fef596d97a90b1b1
Vaibhav Sharma <vaibhav.gtalk@gmail.com>
2021-01-28 19:12:46 +0000

    Added Trello API calls

--- todo_app/app.py
+++ todo_app/app.py
@@ -3,9 +3,19 @@ from flask import Flask, render_template, request, redirect
 from todo_app.flask_config import Config
 
 import todo_app.data.session_items as session_items
+import requests, os, json
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Prepare the API payload, fire API, receive response
+payload = {'fields':'name,url','key':os.environ.get('TRELLO_KEY'),'token':os.environ.get('TRELLO_TOKEN')}
+api_response = requests.get('https://api.trello.com/1/members/vaibhavsharma206/boards',params=payload)
+api_response_str = str(json.dumps(api_response.json()))
+api_response_str_clean = api_response_str.rstrip("]").lstrip("[")
+api_resonse_dict = json.loads(api_response_str_clean)
+# print('Data dict',data["name"])
+
 
 @app.route('/')
 def index():
